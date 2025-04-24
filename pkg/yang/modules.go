627bd948764e0d7f595b7177c86e39383521ad89
Alexander North <alexandernorth@users.noreply.github.com>
2025-04-24 16:00:01 +0200

    fix missing type checks in refine implementation. remove added error checks resulting in duplicate node errors (due to merge function)

--- pkg/yang/modules.go
+++ pkg/yang/modules.go
@@ -371,14 +371,6 @@ func (ms *Modules) Process() []error {
 		}
 	}
 
-	// rerun the error checks, after augmentation happened
-	for _, m := range ms.Modules {
-		errs = append(errs, ToEntry(m).GetErrors()...)
-	}
-	for _, m := range ms.SubModules {
-		errs = append(errs, ToEntry(m).GetErrors()...)
-	}
-
 	if len(errs) > 0 {
 		return errorSort(errs)
 	}
